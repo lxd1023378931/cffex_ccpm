b0fd229d9c26889b3a043dac934fd633fc4cfd09
liangxiudou <1023378931@qq.com>
2018-08-02 23:32:40 +0800

    处理xml

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from src.config import Config
 from src.request import Request
-
+from xml.dom.minidom import parse
+import xml.dom.minidom
 
 class Main(object):
 
@@ -25,4 +26,21 @@ class Main(object):
         url = self.__url.replace("YYYYMM", self.__yyyyMM, 1).replace("DD", self.__dd, 1)
         for type in typeList:
             print(url.replace("TYPE", type, 1))
-            response = self.__request.open(url)
+            response = self.__request.open(url.replace("TYPE", type, 1))
+            if(response.status_code == 200):
+                self.__xml(response.content)
+    def __xml(self,str):
+        domTree = xml.dom.minidom.parseString(str)
+        element = domTree.documentElement
+        datas = element.getElementsByTagName("data")
+        for data in datas:
+            instrumentid = data.getElementsByTagName("instrumentid")[0].childNodes[0].data
+            tradingday = data.getElementsByTagName("tradingday")[0].childNodes[0].data
+            datatypeid = data.getElementsByTagName("datatypeid")[0].childNodes[0].data
+            rank = data.getElementsByTagName("rank")[0].childNodes[0].data
+            shortname = data.getElementsByTagName("shortname")[0].childNodes[0].data
+            volume = data.getElementsByTagName("volume")[0].childNodes[0].data
+            varvolume = data.getElementsByTagName("varvolume")[0].childNodes[0].data
+            partyid = data.getElementsByTagName("partyid")[0].childNodes[0].data
+            productid = data.getElementsByTagName("productid")[0].childNodes[0].data
+            print(tradingday+"-"+instrumentid+":"+rank+"  "+shortname+"  "+volume+"  "+varvolume)
--- src/request.py
+++ src/request.py
@@ -2,9 +2,19 @@ import requests
 
 
 class Request(object):
-
+    headers = {
+        "User-Agent": "ozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36",
+        "Accept": "text/html,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+        "Host": "www.cffex.com.cn",
+        "X-Requested-With": "XMLHttpRequest",
+        "Connection": "keep-alive"
+    }
     def open(self, url):
-        response = requests.get(url)
+        session = requests.session()
+        session.headers = self.headers
+        response = session.get(url)
         print(response.content)
         return response
 
